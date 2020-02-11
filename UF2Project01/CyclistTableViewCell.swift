cbc4f32b35ed524ac982ee5790b72ab2e078b145
gerard <gerardlloreteij@gmail.com>
2020-02-11 19:00:57 +0100

    Ciclistas a medias

---
+++ UF2Project01/CyclistTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CyclistTableViewCell.swift
+//  UF2Project01
+//
+//  Created by dam on 11/02/2020.
+//  Copyright © 2020 Toni Gutiérrez Sánchez . All rights reserved.
+//
+
+import UIKit
+
+class CyclistTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgCyclist: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblLeader: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- UF2Project01/CyclistViewController.swift
+++ UF2Project01/CyclistViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class CyclistViewController: UIViewController {
+class CyclistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var ciclistas:[Ciclista] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        tableView.rowHeight = 170
+        tableView.register(UINib(nibName: "CyclistTableViewCell", bundle: nil), forCellReuseIdentifier: "customCellID")
         
-        // Do any additional setup after loading the view.
     }
 
     override func viewWillAppear(_ animated: Bool){
         navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationController?.navigationBar.topItem?.title = "Ciclistas"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ciclistas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "customCellID", for: indexPath) as! CyclistTableViewCell
+        myCell.imgCyclist.image = UIImage(named: "ciclista")
+        myCell.lblName
     }
     
     /*
